34e7458928b413c38a0094b47dcb98261bcffbdb
raphael meyer <raphael.akira.meyer@gmail.com>
2021-11-03 22:52:04 +0100

    sampling prototype

--- source/main.cc
+++ source/main.cc
@@ -7,6 +7,8 @@
 #include <hardware/spi.h>
 #include <pico/multicore.h>
 
+#include <cmath>
+
 namespace Pin {
 constexpr const uint Clock = 18;
 constexpr const uint MOSI = 19;
@@ -46,22 +48,21 @@ void main_other_core() {
   adc_gpio_init(26);
   adc_select_input(0);
 
-  uint16_t prev = 16;
-  multicore_fifo_push_blocking(prev);
+  uint16_t prev_interval = 1000;
+  multicore_fifo_push_blocking(prev_interval);
 
   for (;;) {
-    auto const value = adc_read();
+    auto const raw_value = adc_read();
 
-    // calibration ?
-    auto const factor = value >> 7;
+    // 12bit value, scale into range 0..32
+    uint32_t const scaled = raw_value >> 7;
 
-    // calculate window size
-    // 0 -> 1us, 1 -> 2us, 2 -> 5us, 3 -> 10us, 4 -> 20us, 5 -> 50us ... 1s
+    uint32_t const sampling_interval = 1 << scaled;
 
-    if (factor != prev) {
-      multicore_fifo_push_blocking(factor);
+    if (sampling_interval != prev_interval) {
+      multicore_fifo_push_blocking(sampling_interval);
     }
-    prev = factor;
+    prev_interval = sampling_interval;
     sleep_ms(100);
   }
 }
@@ -73,45 +74,37 @@ int main() {
       spi0,      Pin::Clock, Pin::MOSI, Pin::ChipSelect, Pin::DataCommand,
       Pin::Reset};
 
-  auto window = multicore_fifo_pop_blocking();
+  auto sampling_interval = multicore_fifo_pop_blocking();
+  auto current_interval = sampling_interval;
 
   PIO const pio = pio0;
   auto const offset = pio_add_program(pio, &sampling_program);
   auto const sm = pio_claim_unused_sm(pio, true);
-  auto const div = clock_get_hz(clk_sys) / 1.0f;
+  auto const div = clock_get_hz(clk_sys) / 1'000'000.0f;
 
   sampling_program_init(pio, sm, offset, Pin::InBase, div);
+
+  uint16_t const color_1 = 0xffe0;
+  uint16_t const color_2 = 0xf81f;
+
+  uint8_t const offset_1 = 20;
+  uint8_t const offset_2 = 50;
 
   uint8_t x = 0;
   bool prev_1 = false;
   bool prev_2 = false;
 
-  uint8_t f_x_ = 0;
-
   pio_sm_set_enabled(pio, sm, true);
 
-  // analyse adc
-  display.set_window(0, 79, 128, 1);
-  for (uint i = 0; i < 128; ++i) {
-    display.colorize_next_pixel(0x8410);
-  }
-  display.set_window(0, 112, 128, 1);
-  for (uint i = 0; i < 128; ++i) {
-    display.colorize_next_pixel(0x8410);
-  }
+  enum class State { Idle, Sample, Wait };
+  State state = State::Idle;
+
+  int wait_count = 0;
+  uint32_t sampling_count = 0;
 
   for (;;) {
     if (multicore_fifo_rvalid()) {
-      window = multicore_fifo_pop_blocking();
-
-      // analyse adc
-      display.set_window(f_x_, 80, 1, 32);
-      for (uint i = 0; i < 31; ++i) {
-        display.colorize_next_pixel(window == i ? 0x07e0 : 0x0000);
-      }
-      display.colorize_next_pixel(window >= 32 ? 0xf800 : 0x0000);
-
-      f_x_ = (f_x_ + 1) & 0x7f;
+      sampling_interval = multicore_fifo_pop_blocking();
     }
 
     auto const value = pio_sm_get_blocking(pio, sm);
@@ -119,15 +112,45 @@ int main() {
     bool const value_1 = value & 0x1;
     bool const value_2 = value & 0x2;
 
-    draw_signal(display, x, 20, value_1, prev_1, 0xffe0);
-    draw_signal(display, x, 50, value_2, prev_2, 0xf81f);
+    if (state == State::Idle) {
+      if (not prev_1 and value_1) {
+        draw_signal(display, 0, offset_1, prev_1, prev_1, color_1);
+        draw_signal(display, 0, offset_2, prev_2, prev_2, color_2);
+        draw_signal(display, 1, offset_1, value_1, prev_1, color_1);
+        draw_signal(display, 1, offset_2, value_2, prev_2, color_2);
+        state = State::Sample;
+        x = 2;
+        current_interval = sampling_interval;
+        sampling_count = 0;
+      }
+      prev_1 = value_1;
+      prev_2 = value_2;
+    } else if (state == State::Sample) {
+      ++sampling_count;
+      if (sampling_count >= current_interval) {
+        draw_signal(display, x, offset_1, value_1, prev_1, color_1);
+        draw_signal(display, x, offset_2, value_2, prev_2, color_2);
 
-    prev_1 = value_1;
-    prev_2 = value_2;
+        prev_1 = value_1;
+        prev_2 = value_2;
 
-    ++x;
-    if (x >= 128) {
-      x = 0;
+        sampling_count = 0;
+
+        ++x;
+        if (x >= 128) {
+          x = 0;
+          state = State::Wait;
+          wait_count = 1000;
+        }
+      }
+    } else if (state == State::Wait) {
+      if (wait_count > 0) {
+        --wait_count;
+      } else {
+        state = State::Idle;
+        prev_1 = value_1;
+        prev_2 = value_2;
+      }
     }
   }
 }
