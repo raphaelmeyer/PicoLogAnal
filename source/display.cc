8cded90df82739ba4b719abb87430c37ff94e6ef
raphael meyer <raphael.akira.meyer@gmail.com>
2021-11-11 12:11:36 +0100

    draw solid line on falling edge instead of after

--- source/display.cc
+++ source/display.cc
@@ -7,13 +7,19 @@ Display::Display(Config::Display const &config) : lcd_{config} {
 void Display::draw_signals(Buffer const &data) {
   for (uint channel = 0; channel < 2; ++channel) {
     uint const mask = 1 << channel;
-    bool previous = data.front() & mask;
     uint8_t x = 0;
-    for (auto const value : data) {
-      bool const state = value & mask;
+
+    bool state = data.front() & mask;
+
+    for (auto it = data.cbegin(); it < data.cend(); ++it) {
+      bool const previous = state;
+      state = *it & mask;
+      bool const next =
+          std::next(it) != data.cend() ? *std::next(it) & mask : state;
+
       lcd_.set_window(x, vertical_offset[channel], 1, signal_height);
 
-      if (state != previous) {
+      if (state && (not previous || not next)) {
         for (uint i = 0; i < 10; ++i) {
           lcd_.colorize_next_pixel(color[channel]);
         }
@@ -29,7 +35,6 @@ void Display::draw_signals(Buffer const &data) {
         }
       }
 
-      previous = state;
       ++x;
     }
   }
